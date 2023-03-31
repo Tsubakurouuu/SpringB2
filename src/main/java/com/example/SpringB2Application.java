0ffc4a6a156469c9118110caa60d9e8cfaf6f279
sumiyoshi1102 <smystkt1102@gmail.com>
2023-03-31 18:58:52 +0900

    aop

---
+++ src/main/java/com/example/SpringB2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringB2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringB2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.domain.user.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
+import com.example.repository.UserMapper;
+
+@Service
+public class UserServiceImpl implements UserService {
+	@Autowired
+	public UserMapper mapper;
+
+	@Override
+	public void signup(MUser user) {
+		user.setDepartmentId(1);
+		user.setRole("ROLE_GENERAL");
+		mapper.insertOne(user);
+	}
+
+	@Override
+	public List<MUser> getUsers(MUser user) {
+		return mapper.findMany(user);
+	}
+
+	@Override
+	public MUser getUserOne(String userId) {
+		return mapper.findOne(userId);
+	}
+	
+	@Transactional
+	@Override
+	public void updateUserOne(String userId, String password, String userName) {
+		mapper.updateOne(userId, password, userName);
+		
+		int i = 1 / 0;
+	}
+
+	@Override
+	public void deleteUserOne(String userId) {
+		int count = mapper.deleteOne(userId);
+	}
+}
